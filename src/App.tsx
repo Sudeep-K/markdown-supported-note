48d416182047b35a46c5c3e1e7c7863946666425
Sudeep-K <pong32btl@gmail.com>
2023-06-14 21:46:24 +0545

    new note form created, types for notedata defined and event handler for form added

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,30 @@ import './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Container } from 'react-bootstrap';
 
+import NewNote from './NewNote';
+
+export type Note = {
+  id: string
+} & NoteData
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
+
 function App() {
 
   return (
     <Container className='my-4'>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/new" element={<NewNote />} />
         <Route path="/:id">
           <Route index element={<h1>Index</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
