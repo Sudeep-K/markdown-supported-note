afb2e9bfa8feb3cc03e4babf9ad26e7d77ab4b0b
Sudeep-K <pong32btl@gmail.com>
2023-06-19 23:41:45 +0545

    able to create new note and associate new tags with it

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,21 @@ import './App.css'
 
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Container } from 'react-bootstrap';
-
+import { useMemo } from 'react';
+import { useLocalStorage } from './useLocalStorage';
 import NewNote from './NewNote';
+import { v4 as uuidV4 } from 'uuid';
+
+
+type RawNote = {
+  id: string
+} & RawNoteData
+
+type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
 
 export type Note = {
   id: string
@@ -22,12 +35,33 @@ export type Tag = {
 }
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+  
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return {...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+    })
+  }, [notes, tags])
+
+  const onCreateNote = ({ tags, ...data }: NoteData) => {
+    setNotes(prevNotes => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }
+      ]
+    })
+  }
+
+  const addTag = (tag: Tag) => {
+    setTags(prevTags => [...prevTags, tag])
+  }
 
   return (
     <Container className='my-4'>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<NewNote />} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />} />
         <Route path="/:id">
           <Route index element={<h1>Index</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,15 +1,29 @@
-import React, { FormEvent } from 'react'
+import React, { FormEvent, useRef, useState } from 'react'
 import { Button, Col, Form, Row, Stack } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Creatable from 'react-select/creatable'
+import { NoteData, Tag } from './App'
+import { v4 as uuidV4 } from 'uuid'
 
-const NoteForm = () => {
+type NoteFormProps = {
+    onSubmit: (data: NoteData) => void
+    onAddTag: (tag: Tag) => void
+    availableTags: Tag[]
+}
 
+const NoteForm = ({ onSubmit, onAddTag, availableTags }: NoteFormProps) => {
+    const titleRef = useRef<HTMLInputElement>(null)
+    const markdownRef = useRef<HTMLTextAreaElement>(null)
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([])
     
     const handleSubmit = (e : FormEvent) => {
         e.preventDefault()
 
-
+        onSubmit({
+            title: titleRef.current!.value,
+            markdown: markdownRef.current!.value,
+            tags: selectedTags
+        })
     }
   return (
     <>
@@ -19,19 +33,35 @@ const NoteForm = () => {
                     <Col>
                         <Form.Group controlId="title">
                             <Form.Label>Title</Form.Label>
-                            <Form.Control required />
+                            <Form.Control ref={titleRef} required />
                         </Form.Group>
                     </Col>
                     <Col>
                         <Form.Group controlId="tag">
                             <Form.Label>Tag</Form.Label>
-                            <Creatable isMulti />
+                            <Creatable isMulti
+                            onCreateOption={ label => {
+                                const newTag = { id: uuidV4(), label }
+                                onAddTag(newTag)
+                                setSelectedTags(prevTags => [...prevTags, newTag])
+                            }}
+                            value={selectedTags.map(tag => {
+                                return { label: tag.label, value: tag.id }
+                            })} 
+                            options={availableTags.map(tag => {
+                                return {label: tag.label, value: tag.id }
+                            })}
+                            onChange={ tags => {
+                                setSelectedTags(tags.map( tag => {
+                                    return {label: tag.label, id: tag.value}
+                                })
+                            )}}/>
                         </Form.Group>
                     </Col>
                 </Row>
                 <Form.Group controlId="markdown">
                     <Form.Label>Body</Form.Label>
-                    <Form.Control as="textarea" rows={10} required />
+                    <Form.Control ref={markdownRef} as="textarea" rows={10} required />
                 </Form.Group>
                 <Stack direction='horizontal' gap={2} className='justify-content-end'>
                     <Button type='submit' variant='primary'>Save</Button>
